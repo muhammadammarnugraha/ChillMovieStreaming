c94b0be09226272998c07faba0f474dbb302eb19
muhammadammarnugraha <muhammadammarnugraha@gmail.com>
2026-02-16 00:03:38 +0700

    editing image bg problems

--- src/assets/Pages/Login.jsx
+++ src/assets/Pages/Login.jsx
@@ -10,7 +10,8 @@ import BG from "../Images/loginCinema.jpg"
 
 const Login = () => {
   return (
-    <div className={`flex justify-center items-center w-screen h-screen bg-center bg-cover bg-[${BG}]`}>
+    <div className="flex justify-center items-center w-screen h-screen bg-center bg-cover"
+    style={{ backgroundImage: `url(${BG})` }}>
       <div className=" p-[24px] lg:p-[40px] flex flex-col justify-center bg-[#181A1CD6]/84 min-w-[306px] min-h-[384px] lg:h-[663px] lg:w-[529px] max-w-[529px] max-h-[663px] rounded-[8px] lg:rounded-[16px] gap-[20px] lg:gap-[37px]">
         <Logo pages="login"/>
         <InputForm pages="login"/>
--- src/assets/Pages/Register.jsx
+++ src/assets/Pages/Register.jsx
@@ -8,7 +8,8 @@ import BG from "../Images/registerCinema.jpg"
 
 const Register = () => {
   return (
-    <div className={`flex justify-center items-center w-screen h-screen bg-center bg-cover bg-[${BG}]`}>
+    <div className="flex justify-center items-center w-screen h-screen bg-center bg-cover"
+    style={{ backgroundImage: `url(${BG})` }}>
       <div className=" p-[24px] lg:p-[40px] flex flex-col justify-center bg-[#181A1CD6]/84 min-w-[306px] min-h-[384px] lg:h-[778px] lg:w-[529px] max-w-[529px] max-h-[778px] rounded-[8px] lg:rounded-[16px] gap-[20px] lg:gap-[37px]">
         <Logo pages="register"/>
         <InputForm pages="register"/>
