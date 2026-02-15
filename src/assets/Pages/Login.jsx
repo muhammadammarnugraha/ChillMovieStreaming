5717f4d2eb50233a9e0eaafcd6c42c08917442c0
muhammadammarnugraha <muhammadammarnugraha@gmail.com>
2026-02-15 23:53:37 +0700

    modified image

--- src/assets/Pages/Login.jsx
+++ src/assets/Pages/Login.jsx
@@ -4,12 +4,13 @@ import Input from "../Atoms/Input";
 import InputForm from "../Molecules/InputForm";
 import SubmitForm from "../Molecules/SubmitForm";
 import Logo from "../Molecules/Logo";
+import BG from "../Images/loginCinema.jpg"
 
 // import Button from '../Atoms/Button'
 
 const Login = () => {
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-center bg-cover bg-[url(C:\Users\AMMAR\harisenin.com\ChillMovieStreaming_React\ChillMovieStreaming\src\assets\Images\loginCinema.jpg)]">
+    <div className={`flex justify-center items-center w-screen h-screen bg-center bg-cover bg-[${BG}]`}>
       <div className=" p-[24px] lg:p-[40px] flex flex-col justify-center bg-[#181A1CD6]/84 min-w-[306px] min-h-[384px] lg:h-[663px] lg:w-[529px] max-w-[529px] max-h-[663px] rounded-[8px] lg:rounded-[16px] gap-[20px] lg:gap-[37px]">
         <Logo pages="login"/>
         <InputForm pages="login"/>
--- src/assets/Pages/Register.jsx
+++ src/assets/Pages/Register.jsx
@@ -4,10 +4,11 @@ import Input from "../Atoms/Input";
 import InputForm from "../Molecules/InputForm";
 import SubmitForm from "../Molecules/SubmitForm";
 import Logo from "../Molecules/Logo";
+import BG from "../Images/registerCinema.jpg"
 
 const Register = () => {
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-center bg-cover bg-[url(C:\Users\AMMAR\harisenin.com\ChillMovieStreaming_React\ChillMovieStreaming\src\assets\Images\registerCinema.jpg)]">
+    <div className={`flex justify-center items-center w-screen h-screen bg-center bg-cover bg-[${BG}]`}>
       <div className=" p-[24px] lg:p-[40px] flex flex-col justify-center bg-[#181A1CD6]/84 min-w-[306px] min-h-[384px] lg:h-[778px] lg:w-[529px] max-w-[529px] max-h-[778px] rounded-[8px] lg:rounded-[16px] gap-[20px] lg:gap-[37px]">
         <Logo pages="register"/>
         <InputForm pages="register"/>
