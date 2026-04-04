b5175b693c5e79b40296cde4579fe87effb4833e
muhammadammarnugraha <muhammadammarnugraha@gmail.com>
2026-04-04 17:29:22 +0700

    adding min-h-fit

--- src/assets/Templates/HomeTemplates.jsx
+++ src/assets/Templates/HomeTemplates.jsx
@@ -6,7 +6,7 @@ import Footer from "../Organisms/Footer";
 
 const HomeTemplates = () => {
   return (
-    <div className="relative w-[100%] h-[1476px] min-w-[320px] min-h-fit lg:w-[100%] lg:h-[100%] lg:min-w-[1024px] lg:min-h-[3324px] max-w-none max-h-[3324px]">
+    <div className="relative w-[100%] min-w-[320px] min-h-fit lg:w-[100%] lg:h-[100%] lg:min-w-[1024px] max-w-none max-h-[3324px]">
       <Header />
       <Hero />
       <Carrousel title={"Melanjutkan Tonton Film"} isHorizontal={true}/>
